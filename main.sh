9c35e05bda65c68f4981d0a61896f0533f842c67
HenglinShi <string.shi@yahoo.com>
2017-08-28 15:55:17 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,11 @@
 #Install FFmpeg
-
+wget https://www.ffmpeg.org/releases/ffmpeg-2.8.12.tar.gz
+tar -xzvf ffmepg-2.8.12.tar.gz
+cd ffmpeg-2.8.12
+./configure --enable-shared --enable-gpl --enable-swscale --prefix=$HOME/local
+make
+make install
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$HOME/local/lib/pkgconfig
 
 
 #Install opencv
@@ -13,3 +19,28 @@ cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=$HOME/local -D BUILD_opencv_gpu=OFF -DCUDA_HOST_COMPILER=/usr/bin/g++ -D CUDA_GENERATION=Kepler ..
 make -j32
 make install
+
+#安装gflags完成后，再安装glog。其它顺序随意
+#install gflags-1.7
+wget https://github.com/gflags/gflags/archive/v1.7.tar.gz
+tar -xzvf v1.7.tar.gz
+cd gflags-1.7
+./configure --prefix=$HOME/local
+make -j32
+make install
+
+#install glog 0.3.3
+wget https://github.com/google/glog/archive/v0.3.3.tar.gz
+tar -xzvf v0.3.3.tar.gz
+cd glog-0.3.3
+./configure --prefix=$HOME/local
+make -j32
+make install
+
+
+#install protobuf 2.5.0
+
+#install boost 1.55.0
+
+#install hdf5
+
